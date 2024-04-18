6b489ff048c259495ae07add0cf823551cdb6e98
christindbose <chris241@purdue.edu>
2024-04-17 23:25:59 -0400

    cmd to run rm1

--- run_rm1.sh
+++ run_rm1.sh
@@ -2,16 +2,16 @@ PyGenTbl='import sys; rows,tables=sys.argv[1:3]; print("-".join([rows]*int(table
 PyGetCore='import sys; c=int(sys.argv[1]); print(",".join(str(2*i) for i in range(c)))'
 PyGetHT='import sys; c=int(sys.argv[1]); print(",".join(str(2*i + off) for off in (0, 48) for i in range(c)))'
 
-DLRM_SYSTEMS=$PWD
-MODELS_PATH=$PWD
+DLRM_SYSTEMS=/home/tgrogers-raid/a/chris241/DLRM/dlrm_syn/dlrm
+MODELS_PATH=/home/tgrogers-raid/a/chris241/DLRM/dlrm_syn/dlrm
 
 #### Hyper Parameters that might need to change
 REUSE_LEVEL='low' #'medium', 'high'
 NUM_BATCH=120
-BS=32 #8192
+BS=16384 #8192
 LOG=print_out.log
 INSTANCES=1
-DLRM_SYSTEMS='/home/tgrogers-raid/a/liu2550/DLRM_synthetic'
+DLRM_SYSTEMS='/home/tgrogers-raid/a/chris241/accelsim_work/mgpu/DLRM_synthetic'
 
 #### Model Parameters
 BOT_MLP=2048-2048-256-64
@@ -28,7 +28,7 @@ for e in $EMBS; do
     EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
     DATA_GEN="prod,$DLRM_SYSTEMS/datasets/reuse_$REUSE_LEVEL/table_500K.txt,$EMB_ROW"
 
-    python3.7 dlrm_s_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 \
+    python3.7 dlrm_s_uvm_pytorch.py --data-generation=$DATA_GEN --round-targets=True --learning-rate=1.0 --arch-mlp-bot=$BOT_MLP --arch-mlp-top=$TOP_MLP --arch-sparse-feature-size=$EMB_DIM --max-ind-range=40000000 \
             --numpy-rand-seed=727 --num-batches=$NUM_BATCH --data-size 100000000 --num-indices-per-lookup=$EMB_LS --num-indices-per-lookup-fixed=True --arch-embedding-size=$EMB_TBL --print-freq=1 --print-time --mini-batch-size=$BS $EXTRA_FLAGS \
-            --use-gpu --break-point=10
+            --use-gpu --break-point=0
 done
